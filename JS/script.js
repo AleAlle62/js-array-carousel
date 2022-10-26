c992920518f618b49dfa00c6cae4181f80ad26a4
AleAlle62 <basketallegrini@gmail.com>
2022-10-26 18:34:04 +0200

    end + bonus 1

--- JS/script.js
+++ JS/script.js
@@ -10,7 +10,7 @@ const eleSlider = document.querySelector('.slider');
 const eleBtnLeft = document.querySelector('.btn-left');
 const eleBtnRight = document.querySelector('.btn-right');
 
-// creare i tag immagine nell'html
+
 for (let i = 0; i < arrImages.length; i++) {
 	const eleImg = document.createElement('img');
 	eleImg.src = arrImages[i];
@@ -23,58 +23,44 @@ for (let i = 0; i < arrImages.length; i++) {
 
 
 const listEleImg = document.querySelectorAll('.slider-img'); 
-
 let activeIndex = 0;
 
-// aggiungere gli event listeners ai due bottoni
+
+// BOTTONE SOTTO
 eleBtnRight.addEventListener('click', function () {
-	// togliere la classe active dall'elemento attivo corrente
 	listEleImg[activeIndex].classList.remove('active');
 
-
     // ciclo infinto
-
     if (activeIndex === arrImages.length - 1){
         activeIndex = 0
     } else {
         activeIndex++
     }
 
-	// incrementare l'active index
-	// activeIndex++;
-	// aggiungere la classe active all'elemento successivo
-
 	listEleImg[activeIndex].classList.add('active');
 
 	eleBtnLeft.classList.remove('hidden');
-	if (activeIndex === listEleImg.length - 1) {
+	if (activeIndex === listEleImg.length + 1) {
 		eleBtnRight.classList.add('hidden');
 	}
 });
 
+// BOTTONE SOPRA 
 eleBtnLeft.addEventListener('click', function () {
-	// togliere la classe active dall'elemento attivo corrente
 	listEleImg[activeIndex].classList.remove('active');
 
-
-    // ciclo infinito
-    
-    if (activeIndex === 0){
-        activeIndex = listEleImg.length - 1
+    // ciclo infinto
+    if (activeIndex === arrImages.length - 1){
+        activeIndex = 0
     } else {
-        activeIndex--
+        activeIndex++
     }
-
-	// incrementare l'active index
-	
-
-	// aggiungere la classe active all'elemento successivo
 	listEleImg[activeIndex].classList.add('active');
 
-	// eleBtnRight.classList.remove('hidden');
-	// if (activeIndex === 0) {
-	// 	eleBtnLeft.classList.add('hidden');
-	// }
+	eleBtnRight.classList.remove('hidden');
+	if (activeIndex === listEleImg.length + 1) {
+		eleBtnLeft.classList.add('hidden');
+	}
 });
 
 
